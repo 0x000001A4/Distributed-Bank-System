8df343b22a3a2146bd3ff92e28581e856d71be73
Afonso <afonsofpinto@gmail.com>
2022-10-10 23:33:06 +0100

    Propose-Sending Propose to Boney

--- BoneyServer/domain/Proposer.cs
+++ BoneyServer/domain/Proposer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,11 +21,14 @@ namespace BoneyServer.domain
             _boneyAdress = boneyAdress;
 
 
+            foreach(String address in _boneyAdress)
+            {
+                GrpcChannel channel = GrpcChannel.ForAddress(address);
+                PaxosAcceptorService.PaxosAcceptorServiceClient client = new PaxosAcceptorService.PaxosAcceptorServiceClient(channel);
+                client.Prepare(new PrepareReq { LeaderNumber = _sourceLeaderNumber, PaxosInstance = (uint)_instance });
 
 
-
-
-
+            }
 
 
         }
